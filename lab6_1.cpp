db9e1af5bf2743aa84c43117d92331c683cf52eb
gunxroswar <gxz.gunforce@gmail.com>
2021-11-26 17:00:07 +0700

    "finished lab6"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+    int N,odd=0,even=0 ;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    while(N!=0)
+        {
+            if(N%2 == 0){
+                even +=1;
+            }
+            else{
+                odd+=1;
+            }
+        cout << "Enter an integer: ";
+        cin >> N;
+        }
+    
+    cout << "#Even numbers = " << even <<"\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
